033fa844d7d6a6cf06ac204d288a1ade7f7d4d20
victor-jo <victor-jo@github.com>
2021-12-27 21:00:02 +0900

    #백준, 단지번호 붙이기 풀이

--- src/boj/dfs/단지번호붙이기.java
+++ src/boj/dfs/단지번호붙이기.java
@@ -1,21 +1,74 @@
-package boj;
+package boj.dfs;
 
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.*;
 
 
 public class 단지번호붙이기 {
+//public class Main {
 
-	public static void main(String[] args) {
+	private static int jarNumber = 0;
+	private static int count = 0;
+	private static int N;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 
+		N = Integer.parseInt(br.readLine());
 
+		int[][] map = new int[N][N];
+		boolean[][] visit = new boolean[N][N];
+		for (int i = 0; i < N; ++i) {
+			String s = br.readLine();
+			for (int j = 0; j < s.toCharArray().length; ++j) {
+				map[i][j] = s.charAt(j) - '0';
+			}
+		}
 
+		List<Integer> countList = new ArrayList<>();
+
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < N; ++j) {
+				if (map[i][j] == 1 && !visit[i][j]) {
+					++jarNumber;
+					count = 0;
+					dfs(map, visit, i, j);
+					countList.add(count);
+				}
+			}
+		}
+
+		bw.write(String.valueOf(jarNumber));
+		bw.newLine();
+		Collections.sort(countList);
+		for (int i = 0; i < jarNumber; ++i) {
+			bw.write(String.valueOf(countList.get(i)));
+			bw.newLine();
+		}
+		bw.flush();
+	}
+
+	private static final int[] dx = {1, -1, 0, 0};
+	private static final int[] dy = {0, 0, -1, 1};
+
+	private static void dfs(int[][] map, boolean[][] visit, int i, int j) {
+		if (!(i >= 0 && i < N && j >= 0 && j < N)) {
+			return;
+		}
+
+		if (visit[i][j] || map[i][j] == 0) {
+			return;
+		}
+
+		visit[i][j] = true;
+		++count;
+
+		for (int k = 0; k < 4; ++k) {
+			dfs(map, visit, i + dx[k], j + dy[k]);
+		}
 	}
 
 }
