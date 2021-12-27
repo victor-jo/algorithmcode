0f2b4b6863557853593ae1345ee83741897c13bb
victor-jo <victor-jo@github.com>
2021-12-27 20:56:25 +0900

    #백준, 단지번호 붙이기 풀이

---
+++ src/boj/dfs/단지번호붙이기.java
@@ -0,0 +1,21 @@
+package boj;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class 단지번호붙이기 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+
+
+	}
+
+}
