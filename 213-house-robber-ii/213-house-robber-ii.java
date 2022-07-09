bca5890b83917554e9087cf063342056c8318501
조원준 <71342955+victor-jo@users.noreply.github.com>
2022-07-09 20:43:56 +0900

    Time: 1 ms (37.15%), Space: 41.3 MB (63.72%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int robs(int[] nums) {        
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        dp[2] = Math.max(dp[0]+nums[2], nums[1]);
+        for (int i=3; i<nums.length; ++i) {
+            dp[i] = Math.max(dp[i], dp[i-3]+nums[i]);
+            dp[i] = Math.max(dp[i], dp[i-2]+nums[i]);
+            dp[i] = Math.max(dp[i], dp[i-1]);
+        } 
+        
+        return dp[nums.length-1];
+    }
+    
+    public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        
+        if (nums.length == 3) {
+            return Math.max(nums[0], Math.max(nums[1], nums[2]));
+        }
+        
+        int max = 0;
+        max = Math.max(max, robs(Arrays.copyOfRange(nums, 0, nums.length-1)));
+        max = Math.max(max, robs(Arrays.copyOfRange(nums, 1, nums.length)));
+        return max;
+    }
+}
