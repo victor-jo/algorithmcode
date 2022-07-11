68e98e4e99e0bf0011f459ce915d02d5918e12f7
조원준 <71342955+victor-jo@users.noreply.github.com>
2022-07-11 23:24:31 +0900

    Time: 4 ms (46.91%), Space: 67.8 MB (53.71%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,16 +1,9 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int i = nums.length - 2;
-        while (i >= 0) {
-            if (nums[i] == 0) {
-                int dist = 0;
-                while (nums[i] <= dist) {
-                    ++dist;
-                    --i;
-                    if (i < 0) return false;
-                }
-            }
-            --i;
+        int maxIndex = 0;
+        for (int i=0; i<nums.length-1; ++i) {
+            if (nums[i] == 0 && maxIndex <= i) return false;
+            maxIndex = Math.max(maxIndex, i+nums[i]);
         }
         return true;
     }
