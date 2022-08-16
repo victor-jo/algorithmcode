b8e48a488319c391615012a851bf21e2be9b0fda
조원준 <71342955+victor-jo@users.noreply.github.com>
2022-08-16 22:27:39 +0900

    Time: 13 ms (73.10%), Space: 44.3 MB (74.61%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,69 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
         
-        int[] stack = new int[32];
-        int top = -1;
-        
-        int length = getLength(x);
-        int mid = length / 2;
-        
-        if (length == 1) {
-            return true;
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
         }
         
-        if ((length % 2) == 0) { // even
-            for (int i=0; i<length; ++i) {
-                if (mid <= i) {
-                    if (stack[top--] != (x % 10)) {
-                        return false;
-                    }
-                }
-                else {
-                    stack[++top] = (x % 10);
-                }
-                x /= 10;
-            }
-        }
-        else { // odd
-            for (int i=0; i<length; ++i) {
-                if (mid == i) {
-                    // ignore
-                }
-                else if (mid < i) {
-                    if (stack[top--] != (x % 10)) {
-                        return false;
-                    }
-                }
-                else {
-                    stack[++top] = (x % 10);
-                }
-                x /= 10;
-            }
-        }
-        
-        return true;
-    }
-    
-    public int getLength(int number) {
-        int length = 1;
-        if (number >= 100000000) {
-            length += 8;
-            number /= 100000000;
-        }
-        if (number >= 10000) {
-            length += 4;
-            number /= 10000;
-        }
-        if (number >= 100) {
-            length += 2;
-            number /= 100;
-        }
-        if (number >= 10) {
-            length += 1;
-        }
-        return length;
+        return revertedNumber == x || x == revertedNumber / 10;
     }
 }
