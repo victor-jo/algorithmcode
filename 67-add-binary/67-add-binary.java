7d1aac29de57c075a22cf0828f6d023815959255
조원준 <71342955+victor-jo@users.noreply.github.com>
2022-08-21 14:04:23 +0900

    Time: 2 ms (91.92%), Space: 42.6 MB (69.38%) - LeetHub

--- 67-add-binary/67-add-binary.java
+++ 67-add-binary/67-add-binary.java
@@ -6,14 +6,27 @@ class Solution {
         int len2 = b.length() - 1;
         
         int carry = 0;
+        int total = 0;
         
         while (len1 >= 0 || len2 >= 0) {
             int v1 = len1 >= 0 ? a.charAt(len1--) - '0' : 0;
             int v2 = len2 >= 0 ? b.charAt(len2--) - '0' : 0;
             
-            int total = v1 + v2 + carry;
-            sb.append(total % 2);
-            carry = total / 2;
+            total = v1 + v2 + carry;
+            
+            if (total == 3) {
+                total = 1;
+                carry = 1;
+            }
+            else if (total == 2) {
+                total = 0;
+                carry = 1;
+            }
+            else {
+                carry = 0;
+            }
+            
+            sb.append(total);
         }
         
         if (carry == 1) {
