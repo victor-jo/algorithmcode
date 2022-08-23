15816c45f525e0c31261acf116c6c045ce2017d9
조원준 <71342955+victor-jo@users.noreply.github.com>
2022-08-23 13:08:48 +0900

    Time: 1 ms (39.44%), Space: 43 MB (29.44%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,18 +1,32 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int i = 0;
-        while (i < matrix.length && matrix[i][0] <= target) {
-            ++i;
-        }
-        if (i != 0) {
-            --i;
-        }
-        
-        for (int j=0; j<matrix[i].length; ++j) {
-            if (matrix[i][j] == target) {
-                return true;
+        for (int i=0; i<matrix.length; ++i) {
+            int length = matrix[i].length;
+            if (matrix[i][0] <= target && matrix[i][length-1] >= target) {
+                return searchBinarySearch(matrix[i], target);
             }
         }
+        return false;
+    }
+    
+    public boolean searchBinarySearch(int[] arr, int target) {
+        int begin = 0;
+        int end = arr.length - 1;
+        
+        while (begin <= end) {
+            int mid = (begin + end) / 2;
+            
+            if (arr[mid] == target) {
+                return true;
+            }
+            else if (arr[mid] > target) {
+                end = mid - 1;
+            }
+            else {
+                begin = mid + 1;
+            }
+        }
+        
         return false;
     }
 }
