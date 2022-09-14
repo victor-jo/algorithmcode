6759480a949812e685668ddbbddecc0378639dd2
조원준 <71342955+victor-jo@users.noreply.github.com>
2022-09-14 11:11:36 +0900

    Time: 2 ms (100.00%), Space: 51.2 MB (87.24%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,17 +1,15 @@
 class Solution {
-    boolean[][] visit;
     int n, m;
     
     public int numIslands(char[][] grid) {
         n = grid.length;
         m = grid[0].length;
-        visit = new boolean[n][m];
         
         int count = 0;
         
         for (int i=0; i<n; ++i) {
             for (int j=0; j<m; ++j) {
-                if (grid[i][j] == '1' && !visit[i][j]) {
+                if (grid[i][j] == '1') {
                     dfs(grid, i, j);
                     count += 1;
                 }
@@ -24,11 +22,11 @@ class Solution {
         if (i < 0 || i >= n || j < 0 || j >= m) {
             return;
         }
-        if (grid[i][j] != '1' || visit[i][j]) {
+        if (grid[i][j] != '1') {
             return;
         }
         
-        visit[i][j] = true;
+        grid[i][j] = 'X';
         
         dfs(grid, i+1, j);
         dfs(grid, i-1, j);
