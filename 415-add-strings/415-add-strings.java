50bfe6e38dc2733f9306a8d03f56f6410e94e70c
조원준 <71342955+victor-jo@users.noreply.github.com>
2022-09-15 09:08:38 +0900

    Time: 3 ms (84.91%), Space: 42.8 MB (82.83%) - LeetHub

---
+++ 415-add-strings/415-add-strings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while (i >= 0 || j >= 0) {
+            int val1 = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int val2 = j >= 0 ? num2.charAt(j) - '0' : 0;
+            
+            int sum = (val1 + val2 + carry) % 10;
+            carry = (val1 + val2 + carry) / 10;
+            
+            sb.append(sum);
+            
+            --i;
+            --j;
+        }
+        
+        if (carry > 0) {
+            sb.append(carry);
+        }
+        return sb.reverse().toString();
+    }
+}
