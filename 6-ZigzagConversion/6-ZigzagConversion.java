16a119e22f7c3c771a7e3d71e067da538489091a
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:29:01 +0900

    Create 6-ZigzagConversion

---
+++ 6-ZigzagConversion/6-ZigzagConversion.java
@@ -0,0 +1,26 @@
+// Last updated: 2026. 7. 16. 오전 11:29:00
+class Solution {
+    public String convert(String s, int numRows) {
+        StringBuilder[] stringBuilders = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            stringBuilders[i] = new StringBuilder();
+        }
+
+        int i = 0;
+
+        while (i < s.length()) {
+            for (int j = 0; j < numRows && i < s.length(); j++) {
+                stringBuilders[j].append(s.charAt(i++));
+            }
+            for (int j = numRows-2; j >= 1 && i < s.length(); j--) {
+                stringBuilders[j].append(s.charAt(i++));
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder stringBuilder : stringBuilders) {
+            result.append(stringBuilder.toString());
+        }
+        return result.toString();
+    }
+}
