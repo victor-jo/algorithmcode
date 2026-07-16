90090772140260f1dbca03645a6ff40bf6b435f7
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:28:34 +0900

    Create 41-FirstMissingPositive

---
+++ 41-FirstMissingPositive/41-FirstMissingPositive.java
@@ -0,0 +1,17 @@
+// Last updated: 2026. 7. 16. 오전 11:28:34
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int num : nums) {
+            hashSet.add(num);
+        }
+        
+        for (int i=1; i<=nums.length; ++i) {
+            if (!hashSet.contains(i)) {
+                return i;
+            }
+        }
+        
+        return nums.length + 1;
+    }
+}
