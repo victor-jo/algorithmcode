20310b88857a6eaaa24ba9e5ad70e6fe8d12c22f
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:27:01 +0900

    Create 322-CoinChange

---
+++ 322-CoinChange/322-CoinChange.java
@@ -0,0 +1,22 @@
+// Last updated: 2026. 7. 16. 오전 11:27:01
+class Solution {
+    
+    public int coinChange(int[] coins, int amount) {
+        int inf = 10001;
+        int[] dp = new int[inf];
+        
+        Arrays.fill(dp, inf);
+        
+        dp[0] = 0;
+        
+        for (int i=1; i<=amount; ++i) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        
+        return dp[amount] == inf ? -1 : dp[amount];
+    }
+}
