0b436f3b425109bad4fe5fa1605f11da5c15b929
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:27:56 +0900

    Create 119-PascalsTriangleIi

---
+++ 119-PascalsTriangleIi/119-PascalsTriangleIi.java
@@ -0,0 +1,17 @@
+// Last updated: 2026. 7. 16. 오전 11:27:55
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        Integer[][] arr = new Integer[rowIndex+1][rowIndex+1];
+        for (int i = 0; i <= rowIndex; i++) {
+            for (int j = 0; j < i+1; j++) {
+                if (i == j || j == 0) {
+                    arr[i][j] = 1;
+                }
+                else {
+                    arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+                }
+            }
+        }
+        return List.of(arr[rowIndex]);
+    }
+}
