286ea4b743a8572a04e5e2d0ca81a1c949069065
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:26:50 +0900

    Create 459-RepeatedSubstringPattern

---
+++ 459-RepeatedSubstringPattern/459-RepeatedSubstringPattern.java
@@ -0,0 +1,19 @@
+// Last updated: 2026. 7. 16. 오전 11:26:50
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        for (int i=1; i<=n/2; ++i) {
+            String substring = s.substring(0, i);
+            StringBuilder sb = new StringBuilder();
+            int j = i;
+            while (j <= n) {
+                sb.append(substring);
+                j += i;
+            }
+            if (sb.toString().equals(s)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
