5a80c34b6439e7df92e2123b7d8e73404f5760ce
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:26:12 +0900

    Create 978-ValidMountainArray

---
+++ 978-ValidMountainArray/978-ValidMountainArray.rs
@@ -0,0 +1,14 @@
+// Last updated: 2026. 7. 16. 오전 11:26:12
+impl Solution {
+    pub fn valid_mountain_array(arr: Vec<i32>) -> bool {
+        let mut i = 0;
+        let mut j = arr.len() - 1;
+        while i < arr.len() - 1 && arr[i] < arr[i + 1] {
+            i += 1;
+        }
+        while j > 0 && arr[j] < arr[j - 1] {
+            j -= 1;
+        }
+        i > 0 && i == j && j < arr.len() - 1
+    }
+}
