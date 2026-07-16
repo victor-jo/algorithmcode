54fcf145f38e3e6342f4188639c003e27124180b
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:26:13 +0900

    Create 954-MaximumSumCircularSubarray

---
+++ 954-MaximumSumCircularSubarray/954-MaximumSumCircularSubarray.java
@@ -0,0 +1,29 @@
+// Last updated: 2026. 7. 16. 오전 11:26:13
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        
+        int m = kadane(nums, 0, nums.length, 1);
+        int n1 = kadane(nums, 1, nums.length, -1);
+        int n2 = kadane(nums, 0, nums.length-1, -1);
+        
+        return Math.max(m, Math.max(sum + n1, sum + n2));
+    }
+    
+    public int kadane(int[] nums, int s, int e, int sign) {
+        int result = Integer.MIN_VALUE;
+        int num = Integer.MIN_VALUE;
+        for (int i=s; i<e; ++i) {
+            num = sign * nums[i] + Math.max(num, 0);
+            result = Math.max(result, num);
+        }
+        return result;
+    }
+}
