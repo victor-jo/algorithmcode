13af8ed6705e908bf1f8fe33e777e4d5183f591c
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:25:55 +0900

    Create 1054-ComplementOfBase10Integer

---
+++ 1054-ComplementOfBase10Integer/1054-ComplementOfBase10Integer.java
@@ -0,0 +1,18 @@
+// Last updated: 2026. 7. 16. 오전 11:25:55
+class Solution {
+    public int bitwiseComplement(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int result = 0;
+        int count = 1;
+        while (n != 0) {
+            if ((n & 1) == 0) {
+                result += count;
+            }
+            n >>= 1;
+            count *= 2;
+        }
+        return result;
+    }
+}
