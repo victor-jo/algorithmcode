ce501a2d1809effd5e8e0277c1e85ed422683b2a
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:27:29 +0900

    Create 191-NumberOf1Bits

---
+++ 191-NumberOf1Bits/191-NumberOf1Bits.java
@@ -0,0 +1,12 @@
+// Last updated: 2026. 7. 16. 오전 11:27:29
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            count += (n&1);
+            n >>>= 1;
+        }
+        return count;
+    }
+}
