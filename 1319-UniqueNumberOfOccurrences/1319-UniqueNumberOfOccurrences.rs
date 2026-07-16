917a96af1c1f7b0f8a83988fcc76e51944e7db72
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:25:49 +0900

    Create 1319-UniqueNumberOfOccurrences

---
+++ 1319-UniqueNumberOfOccurrences/1319-UniqueNumberOfOccurrences.rs
@@ -0,0 +1,24 @@
+// Last updated: 2026. 7. 16. 오전 11:25:49
+impl Solution {
+    pub fn unique_occurrences(arr: Vec<i32>) -> bool {
+    
+    use std::collections::HashMap;
+
+    let mut map = HashMap::new();
+    for i in arr {
+        let count = map.entry(i).or_insert(0);
+        *count += 1;
+    }
+
+    use std::collections::HashSet;
+
+    let mut set = HashSet::new();
+    for (_, v) in map {
+        if !set.insert(v) {
+            return false;
+        }
+    }
+
+    return true;
+    }
+}
