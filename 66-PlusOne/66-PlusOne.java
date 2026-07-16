a9cb669d063a4ba26359676fb47a4c873cf4dca0
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:28:20 +0900

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.java
@@ -0,0 +1,17 @@
+// Last updated: 2026. 7. 16. 오전 11:28:20
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int increment = 1;
+        for (int i=digits.length-1; i>=0; --i) {
+            digits[i] = digits[i] + increment;
+            increment = digits[i] / 10;
+            digits[i] = digits[i] % 10;
+        }
+        if (increment == 1) {
+            int[] result = new int[digits.length+1];
+            result[0] = 1;
+            return result;
+        }
+        return digits;
+    }
+}
