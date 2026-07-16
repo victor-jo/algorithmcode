33f87d808422a3b2cbaf6c508a1ab4d9cdb266ae
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:26:28 +0900

    Create 733-FloodFill

---
+++ 733-FloodFill/733-FloodFill.java
@@ -0,0 +1,20 @@
+// Last updated: 2026. 7. 16. 오전 11:26:28
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int color = image[sr][sc];
+        if (color != newColor) {
+            floodFill(image, sr, sc, color, newColor);
+        }
+        return image;
+    }
+
+    private void floodFill(int[][] image, int sr, int sc, int color, int newColor) {
+        if (image[sr][sc] == color) {
+            image[sr][sc] = newColor;
+            if (sr > 0) floodFill(image, sr-1, sc, color, newColor);
+            if (sc > 0) floodFill(image, sr, sc-1, color, newColor);
+            if (sr+1 < image.length) floodFill(image, sr+1, sc, color, newColor);
+            if (sc+1 < image[0].length) floodFill(image, sr, sc+1, color, newColor);
+        }
+    }
+}
