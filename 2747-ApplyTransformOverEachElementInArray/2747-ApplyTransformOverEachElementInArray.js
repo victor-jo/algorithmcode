c5f4169b2e1788b89e44b18a8072b2293346fed7
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:25:04 +0900

    Create 2747-ApplyTransformOverEachElementInArray

---
+++ 2747-ApplyTransformOverEachElementInArray/2747-ApplyTransformOverEachElementInArray.js
@@ -0,0 +1,9 @@
+// Last updated: 2026. 7. 16. 오전 11:25:04
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    return arr.map(fn);
+};
