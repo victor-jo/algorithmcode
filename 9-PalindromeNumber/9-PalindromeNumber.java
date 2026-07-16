6af340cf6944fa3dc20869eb1c49d53e5301dee8
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:28:57 +0900

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,16 @@
+// Last updated: 2026. 7. 16. 오전 11:28:57
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        
+        return revertedNumber == x || x == revertedNumber / 10;
+    }
+}
