0d50f73fcc68af8edbc85fd8163c0f243d24f44b
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:26:11 +0900

    Create 965-UniqueEmailAddresses

---
+++ 965-UniqueEmailAddresses/965-UniqueEmailAddresses.rs
@@ -0,0 +1,16 @@
+// Last updated: 2026. 7. 16. 오전 11:26:11
+impl Solution {
+    pub fn num_unique_emails(emails: Vec<String>) -> i32 {
+        let mut unique_emails = std::collections::HashSet::new();
+        for email in emails {
+            let mut local_name = email.split("@").next().unwrap().to_string();
+            local_name = local_name.replace(".", "");
+            if let Some(index) = local_name.find("+") {
+                local_name = local_name[..index].to_string();
+            }
+            let domain_name = email.split("@").last().unwrap().to_string();
+            unique_emails.insert(format!("{}@{}", local_name, domain_name));
+        }
+        unique_emails.len() as i32   
+    }
+}
