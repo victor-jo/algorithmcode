d480ff3a50a3524fffd0c2199f9e252a83144049
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:26:23 +0900

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,20 @@
+// Last updated: 2026. 7. 16. 오전 11:26:23
+class Solution {
+    public int search(int[] nums, int target) {
+        int begin = 0;
+        int end = nums.length - 1;
+
+        while (begin <= end) {
+            int mid = ((begin + end) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                begin = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
