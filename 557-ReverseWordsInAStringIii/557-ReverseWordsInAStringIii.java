e87e8f198d41d95454af3a831bad6475d607081b
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:26:43 +0900

    Create 557-ReverseWordsInAStringIii

---
+++ 557-ReverseWordsInAStringIii/557-ReverseWordsInAStringIii.java
@@ -0,0 +1,27 @@
+// Last updated: 2026. 7. 16. 오전 11:26:43
+class Solution {
+    public String reverseWords(String s) {
+        char[] words = s.toCharArray();
+        int k = 0;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i] == ' ') {
+                reverse(words, k, i-1);
+                k = i+1;
+            }
+        }
+
+        reverse(words, k, s.length()-1);
+
+        return new String(words);
+    }
+
+    private void reverse(char[] word, int left, int right) {
+        while (left < right) {
+            char temp = word[left];
+            word[left] = word[right];
+            word[right] = temp;
+            ++left;
+            --right;
+        }
+    }
+}
