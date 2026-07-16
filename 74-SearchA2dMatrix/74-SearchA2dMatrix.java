d991178a338dc4481be10823c3e506894429fd61
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:28:14 +0900

    Create 74-SearchA2dMatrix

---
+++ 74-SearchA2dMatrix/74-SearchA2dMatrix.java
@@ -0,0 +1,45 @@
+// Last updated: 2026. 7. 16. 오전 11:28:14
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int begin = 0;
+        int end = matrix.length - 1;
+        
+        while (begin <= end) {
+            int mid = (begin + end) / 2;
+            int length = matrix[mid].length;
+            
+            if (matrix[mid][0] <= target && matrix[mid][length-1] >= target) {
+                return searchBinarySearch(matrix[mid], target);
+            }
+            else if (matrix[mid][0] > target) {
+                end = mid - 1;
+            }
+            else {
+                begin = mid + 1;
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean searchBinarySearch(int[] arr, int target) {
+        int begin = 0;
+        int end = arr.length - 1;
+        
+        while (begin <= end) {
+            int mid = (begin + end) / 2;
+            
+            if (arr[mid] == target) {
+                return true;
+            }
+            else if (arr[mid] > target) {
+                end = mid - 1;
+            }
+            else {
+                begin = mid + 1;
+            }
+        }
+        
+        return false;
+    }
+}
