63767101073d8c2ff18b1a91a708bcf9efe35e86
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:28:19 +0900

    Create 67-AddBinary

---
+++ 67-AddBinary/67-AddBinary.java
@@ -0,0 +1,38 @@
+// Last updated: 2026. 7. 16. 오전 11:28:19
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        
+        int len1 = a.length() - 1;
+        int len2 = b.length() - 1;
+        
+        int carry = 0;
+        int total = 0;
+        
+        while (len1 >= 0 || len2 >= 0) {
+            total = carry;
+            total += len1 >= 0 ? a.charAt(len1--) - '0' : 0;
+            total += len2 >= 0 ? b.charAt(len2--) - '0' : 0;
+            
+            if (total == 3) {
+                total = 1;
+                carry = 1;
+            }
+            else if (total == 2) {
+                total = 0;
+                carry = 1;
+            }
+            else {
+                carry = 0;
+            }
+            
+            sb.append(total);
+        }
+        
+        if (carry == 1) {
+            sb.append(1);
+        }
+        
+        return sb.reverse().toString();
+    }
+}
