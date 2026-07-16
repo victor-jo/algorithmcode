bbd0db3253e253c2ac5658c795341e006e9adc64
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:25:41 +0900

    Create 1584-AverageSalaryExcludingTheMinimumAndMaximumSalary

---
+++ 1584-AverageSalaryExcludingTheMinimumAndMaximumSalary/1584-AverageSalaryExcludingTheMinimumAndMaximumSalary.java
@@ -0,0 +1,15 @@
+// Last updated: 2026. 7. 16. 오전 11:25:41
+class Solution {
+    public double average(int[] salary) {
+        int N = salary.length;
+        int[] sums = new int[N+1];
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for (int i=N-1; i>-1; --i) {
+            sums[i] = sums[i+1] + salary[i];
+            min = Math.min(min, salary[i]);
+            max = Math.max(max, salary[i]);
+        }
+        return (sums[0]-(min+max))/(N-2.0);
+    }
+}
