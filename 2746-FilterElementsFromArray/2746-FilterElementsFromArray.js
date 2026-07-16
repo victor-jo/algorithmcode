b97a0bc04141940d18804ae452a754e1e93aa629
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:25:05 +0900

    Create 2746-FilterElementsFromArray

---
+++ 2746-FilterElementsFromArray/2746-FilterElementsFromArray.js
@@ -0,0 +1,9 @@
+// Last updated: 2026. 7. 16. 오전 11:25:05
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    return arr.filter(fn);
+};
