cb6b85c39ed9bf5e0eb15fe26d848ccbce1ca906
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:26:27 +0900

    Create 740-DeleteAndEarn

---
+++ 740-DeleteAndEarn/740-DeleteAndEarn.java
@@ -0,0 +1,29 @@
+// Last updated: 2026. 7. 16. 오전 11:26:26
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int maxNumber = 0;
+        for (int num : nums) {
+            maxNumber = Math.max(maxNumber, num);
+        }
+        
+        if (maxNumber == 0) {
+            return 0;
+        }    
+        
+        int[] dp = new int[maxNumber+1];
+        for (int num : nums) {
+            dp[num] = dp[num] + num;
+        }
+        
+        int skip = 0;
+        int take = dp[0];
+        
+        for (int i=1; i<=maxNumber; i++) {
+            int temp = skip;
+            skip = Math.max(skip, take);
+            take = temp + dp[i];
+        }
+        
+        return Math.max(skip, take);
+    }
+}
