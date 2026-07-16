f28185f23f7d4c483606b34199ada943573f23bd
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:26:34 +0900

    Create 680-ValidPalindromeIi

---
+++ 680-ValidPalindromeIi/680-ValidPalindromeIi.java
@@ -0,0 +1,28 @@
+// Last updated: 2026. 7. 16. 오전 11:26:33
+class Solution {
+    public boolean validPalindrome(String s) {
+        boolean usedChance = false;
+        return validPalindrome(s, usedChance);
+    }
+    
+    public boolean validPalindrome(String s, boolean usedChance) {
+        int l = 0;
+        int r = s.length() - 1;
+        
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                if (usedChance) {
+                    return false;
+                }
+                return validPalindrome(s.substring(l, r), true)
+                    || validPalindrome(s.substring(l+1, r+1), true);
+            }
+            else {
+                ++l;
+                --r;    
+            }
+        }
+        
+        return true;
+    }
+}
