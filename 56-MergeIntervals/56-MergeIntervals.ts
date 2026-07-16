fe2b9700aa1d57b20201ce06f43cd2cddb8b3a19
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:28:26 +0900

    Create 56-MergeIntervals

---
+++ 56-MergeIntervals/56-MergeIntervals.ts
@@ -0,0 +1,18 @@
+// Last updated: 2026. 7. 16. 오전 11:28:26
+function merge(intervals: number[][]): number[][] {
+  intervals.sort((a: number[], b: number[]): number => a[0] - b[0]);
+  
+  let arr : number[][] = [];
+  let top : number = -1;
+  
+  intervals.forEach(interval => {
+      if (top == -1 || arr[top][1] < interval[0]) {
+          arr[++top] = interval;
+      }
+      else {
+          arr[top] = [arr[top][0], Math.max(arr[top][1], interval[1])];
+      }
+  });
+  
+  return arr;
+};
