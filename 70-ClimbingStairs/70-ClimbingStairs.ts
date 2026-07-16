be62b854a3b6c8f1b0d9a1e097249248a8fc3115
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:28:18 +0900

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.ts
@@ -0,0 +1,10 @@
+// Last updated: 2026. 7. 16. 오전 11:28:18
+function climbStairs(n: number): number {
+  let dp: number[] = [];
+  dp[n] = 1;
+  dp[n-1] = 1;
+  for (let i: number=n-2; i>=0; --i) {
+    dp[i] = dp[i+1] + dp[i+2];
+  }
+  return dp[0];
+};
