e3d69af3db655dab9e12221f17a622e1fd904e7f
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:27:35 +0900

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.java
@@ -0,0 +1,19 @@
+// Last updated: 2026. 7. 16. 오전 11:27:35
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int major = 0;
+        for (int i=0; i<nums.length; ++i) {
+            if (count == 0) {
+                major = nums[i];
+            }
+            if (major == nums[i]) {
+                ++count;
+            }
+            else {
+                --count;
+            }
+        }
+        return major;
+    }
+}
