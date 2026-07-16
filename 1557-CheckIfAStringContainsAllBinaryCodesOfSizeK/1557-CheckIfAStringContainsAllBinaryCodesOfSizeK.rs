b2d717631fbbc912db4ae00458c40f11d3affb52
VictorZo <71342955+victor-jo@users.noreply.github.com>
2026-07-16 11:25:42 +0900

    Create 1557-CheckIfAStringContainsAllBinaryCodesOfSizeK

---
+++ 1557-CheckIfAStringContainsAllBinaryCodesOfSizeK/1557-CheckIfAStringContainsAllBinaryCodesOfSizeK.rs
@@ -0,0 +1,25 @@
+// Last updated: 2026. 7. 16. 오전 11:25:42
+impl Solution {
+    pub fn has_all_codes(s: String, k: i32) -> bool {
+        let limit = 1 << k;
+        let mask = (1 << k) - 1;
+
+        let mut seen = vec![false; limit];
+        let mut hash = 0;
+
+        let mut count = 0;
+
+        for (i, c) in s.chars().enumerate() {
+            hash = ((hash << 1) & mask) | (c as u32 - '0' as u32);
+            if i >= (k - 1_i32) as usize && !seen[hash as usize] {
+                seen[hash as usize] = true;
+                count += 1;
+                if count == limit {
+                    return true;
+                }
+            }
+        }
+
+        false
+    }
+}
